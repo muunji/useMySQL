efbf7ae1b5594bb90deab218e99b228de652bd48
muunji <twoji3019@naver.com>
2025-04-03 14:21:21 +0900

    insert 사용

--- crud.js
+++ crud.js
@@ -6,3 +6,6 @@ const connection = require('./connect.js')
 //MySQL에 삽입할 정보를 미리 설정
 const username = 'newuser'
 const password = 'newpassword'
+
+//INSERT INTO 문법 사용용
+const sql = 'INSERT INTO users (username, password) VALUES (?,?)'
