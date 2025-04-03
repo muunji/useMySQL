43514ebf2fc4b8cfc18a7eb10988c62bf394d1ee
muunji <twoji3019@naver.com>
2025-04-03 12:51:01 +0900

    주석으로 설명 & 내보내기

--- connect.js
+++ connect.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2')
 
+//MySQL과 연결을 설정
 const connection = mysql.createConnection({
   host : 'localhost',
   user:'root',
@@ -7,6 +8,8 @@ const connection = mysql.createConnection({
   database:'test'
 })
 
+
+//MySQL에 연결 시도
 connection.connect(err=>{
   if(err){
     console.error('MySQL 연결 실패:',err)
@@ -14,3 +17,6 @@ connection.connect(err=>{
   }
   console.log('MySQL 연결 성공')
 })
+
+//내보내기
+module.exports = connection
