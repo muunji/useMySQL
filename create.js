58e0da35fb44e4ebc582d09491f053e8458ee958
muunji <twoji3019@naver.com>
2025-04-03 15:53:23 +0900

    insert 작성

--- create.js
+++ create.js
@@ -9,4 +9,11 @@ const password = 'newpassword'
 
 //INSERT INTO 문법 사용용
 const sql = 'INSERT INTO users (username, password) VALUES (?,?)'
-connection
+connection.query(sql, [username,password],(err,results)=>{
+  if(err){
+    console.log('데이터 삽입 실패:',err)
+    return
+  }
+  console.log('데이터 삽입 성공',results.insertId)
+  connection.end()
+})
