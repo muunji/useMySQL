91819763c63bdc6534671c1a7e4507d67b8d515e
muunji <twoji3019@naver.com>
2025-04-03 16:00:24 +0900

    함수 만들기

--- crud.js
+++ crud.js
@@ -1,4 +1,20 @@
 //mysql 가져오기
 const connection = require('./connect.js')
 
-//Create
+//Create 데이터베이스 생성
+const createDB = () => {}
+
+//Create 테이블 생성
+const createTable = () => {}
+
+//데이터 추가
+const insertData = () => {}
+
+//데이터 조회
+const getData = () => {}
+
+//데이터 수정
+const updateData = () => {}
+
+//데이터 삭제
+const deleteData = () => {}
