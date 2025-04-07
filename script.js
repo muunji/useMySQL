e7eeada68ea8ffaf59546adb4efdd58a50883110
muunji <twoji3019@naver.com>
2025-04-07 22:21:23 +0900

    주석으로 과정 작성

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //제출 이벤트
 document.getElementsByTagName('button')[0].addEventListener('submit',(e)=>{
-  
+  //기본 동작 방지
+
+  //입력값 가져오기
+
+  //유효성 검사
+
+  //console로 전송 시뮬레이션
+
+  //input 값 초기화
 })
