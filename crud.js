46f43ca3d10fd0147dc361cd291c10b58aac6a58
muunji <twoji3019@naver.com>
2025-04-07 10:28:00 +0900

    성공했을 시 console 작성

--- crud.js
+++ crud.js
@@ -6,21 +6,8 @@ const createDB = () => {
   connection.query('CREATE DATABASE IF NOT EXISTS test',(err,results)=>{
     if(err){
       console.error('DB 생성 실패',err)
+      return
     }
+    console.log('DB 생성 성공')
   })
 }
-
-//Create 테이블 생성
-const createTable = () => {}
-
-//데이터 추가
-const insertData = () => {}
-
-//데이터 조회
-const getData = () => {}
-
-//데이터 수정
-const updateData = () => {}
-
-//데이터 삭제
-const deleteData = () => {}
