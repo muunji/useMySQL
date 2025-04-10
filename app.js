5de795099efa5ac2d8e5c354c28a79c866c316d6
muunji <twoji3019@naver.com>
2025-04-10 09:11:21 +0900

    express 서버 설정 진행 중

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import express from 'express'
 import mysql from 'mysql2'
 import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+//파일 설정
+app.use(express.static())
