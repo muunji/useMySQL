68b43b3cdfca7296ff9e35d5bb5317180c684500
muunji <twoji3019@naver.com>
2025-04-11 17:58:40 +0900

    연결 & 내보내기 & module 방식으로 수정

--- server/connect.js
+++ server/connect.js
@@ -1,8 +1,7 @@
 //필요한 모듈 가져오기
-const mysql = require('mysql2')
-const pool = require('pool')
-const dotenv = require('dotenv')
-
+import mysql from 'mysql2'
+import pool from 'pool'
+import dotenv from 'dotenv'
 dotenv.config()
 
 const connection = mysql.createPool({
@@ -14,3 +13,15 @@ const connection = mysql.createPool({
   connectionLimit:10, //최대 10개 연결 유지
   queueLimit: 0
 })
+
+//연결하기
+connection.connect(err=>{
+  if(err){
+    console.err('MySQL 연결 실패',err)
+    return
+  }
+  console.log('MySQL 연결 성공')
+})
+
+//내보내기
+export default connection
