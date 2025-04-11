295b93f2f81764a9af3ce10ddd656ea036fa2cb2
muunji <twoji3019@naver.com>
2025-04-11 17:53:47 +0900

    환경 변수 설정 + dotenv 설치

--- server/connect.js
+++ server/connect.js
@@ -1,3 +1,12 @@
 //필요한 모듈 가져오기
 const mysql = require('mysql2')
 const pool = require('pool')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const connection = mysql.createPool({
+  host:'localhost',
+  user:'root',
+  
+})
