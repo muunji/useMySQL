6902f3dc2b2df3a22baf1ad7698146e360d38cac
muunji <twoji3019@naver.com>
2025-04-14 10:56:00 +0900

    오타 수정

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 import path from 'path'
 import { fileURLToPath } from 'url'
 //pool
-import pool from './server/connect,js'
+import pool from './server/connect.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
