d564e91d5c1550a4407e19be82888dff24cb58e3
muunji <twoji3019@naver.com>
2025-04-14 14:27:15 +0900

    포트번호 추가로 함수 사용없이도 사용 가능

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 //pool
 import pool from './server/connect.js'
+// import { createTable } from './server/create.js'
 //DB 함수 
-import { createTable } from './server/create.js'
+// import { createDB,createTable } from './server/create.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -20,8 +21,8 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname,'public')))
 
 //*DB , Table 생성
-// createDB()
-createTable()
+// createDB(createTable())
+// createTable()
 
 //* DB 서버 테스트
 app.get('/check',(req,res)=>{
