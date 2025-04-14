20a3f4f62870be2fa45e7cffd31bcf524aca5ef5
muunji <twoji3019@naver.com>
2025-04-14 14:26:58 +0900

    함수 실행 시점을 위해 콜백 사용

--- server/create.js
+++ server/create.js
@@ -2,11 +2,13 @@
 import connect from './connect.js'
 
 //DB 생성
-const createDB = () => {
+const createDB = (callback) => {
   connect.execute('CREATE DATABASE IF NOT EXISTS express',(err)=>{
     if(err) return console.error('DB 생성 실패:',err)
     console.log('DB 생성 완료')
   })
+
+  callback
 }
 
 //table 생성
