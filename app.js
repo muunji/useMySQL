1469fd275788d5b04f6789cdf9af48110bc88ec5
muunji <twoji3019@naver.com>
2025-04-14 10:55:12 +0900

    조회 쿼리문 작성

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ app.use(express.static(path.join(__dirname,'public')))
 //* DB 서버 테스트
 app.get('/check',(req,res)=>{
   //조회 쿼리 작성
-  
+  pool.execute(`SELECT * FROM text`,(err,results)=>{
+    if(err){
+      console.error('DB 조회 에러',err)
+      return res.status(500).json({error:'DB 오류'})
+    }
+    //오류 없으면
+    res.json(results)
+  })
 })
 
 //서버 실행
