4d6abce8ef98a32600944a586657cb743efa14b6
muunji <twoji3019@naver.com>
2025-04-14 11:46:28 +0900

    동일한 오류 발생 > database 부분 주석 처리

--- server/connect.js
+++ server/connect.js
@@ -8,19 +8,21 @@ const pool = mysql.createPool({
   host:'localhost',
   user:'root',
   password: process.env.PASSWORD,
-  database:'express',
+  // database:'express',
   waitForConnections: true,
   connectionLimit:10, //최대 10개 연결 유지
   queueLimit: 0
 })
 
 //연결하기
-pool.getConnection(err=>{
+pool.getConnection((err,connection)=>{
   if(err){
-    console.error('MySQL 연결 실패',err)
+    console.error('연결실패',err.message)
+    console.error('MySQL 연결 실패, 전체 오류',err)
     return
   }
   console.log('MySQL 연결 성공')
+  connection.release()
 })
 
 //내보내기
