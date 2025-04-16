321ab40d0c3354d67186846338930e067c7c64ca
muunji <twoji3019@naver.com>
2025-04-16 18:03:46 +0900

    추가한 컬럼 라우터에 적용

--- routes/text.js
+++ routes/text.js
@@ -21,7 +21,7 @@ router.post('/',(req,res)=>{
   //값이 없으면 에러 처리
   if(!text) return res.status(400).json({error:'내용이 필요합니다'})
   //쿼리문 사용
-  pool.execute(`INSERT INTO test (text,password) VALUES (?)`,[text,password],(err,results)=>{
+  pool.execute(`INSERT INTO test (text,password) VALUES (?,?)`,[text,password],(err,results)=>{
     if(err) return res.status(500).json({message:'DB 추가 실패',error:err})
     res.json(results)
   })
