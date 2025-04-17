2da8c8420f9816e9cda925c54f4d41473d1217e4
muunji <twoji3019@naver.com>
2025-04-17 09:46:56 +0900

    hash 사용과 try,catch 안에 코드 옮기기 및 작성

--- routes/text.js
+++ routes/text.js
@@ -26,13 +26,16 @@ router.post('/',async (req,res)=>{
   if(!text) return res.status(400).json({error:'내용이 필요합니다'})
   if(!password) return res.status(400).json({error:'비밀번호 필요합니다'})
 
-  try{}
-  catch(err){}
-  //쿼리문 사용
-  pool.execute(`INSERT INTO test (text,password) VALUES (?,?)`,[text,password],(err,results)=>{
-    if(err) return res.status(500).json({message:'DB 추가 실패',error:err})
-    res.json(results)
-  })
+  try{
+    //해시화
+    const hashPassword = await bcrypt.hash(password,10)
+    //쿼리문 사용
+    pool.execute(`INSERT INTO test (text,password) VALUES (?,?)`,[text,hashPassword])
+    res.json({message:'비밀번호 추가 완료'})
+  }
+  catch(err){
+    return res.status(500).json({message:'DB 추가 실패',error:err})
+  }
 })
 
 //PUT - 사용자 수정
