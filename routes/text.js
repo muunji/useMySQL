791185b9e28092105cc3bf5b869f3ecfff70bf3c
muunji <twoji3019@naver.com>
2025-04-17 09:29:38 +0900

    값이 없을 때 에러처리

--- routes/text.js
+++ routes/text.js
@@ -23,6 +23,7 @@ router.post('/',(req,res)=>{
   const {text,password} = req.body
   //값이 없으면 에러 처리
   if(!text) return res.status(400).json({error:'내용이 필요합니다'})
+  if(!password) return res.status(400).json({error:'비밀번호 필요합니다'})
   //쿼리문 사용
   pool.execute(`INSERT INTO test (text,password) VALUES (?,?)`,[text,password],(err,results)=>{
     if(err) return res.status(500).json({message:'DB 추가 실패',error:err})
