9d4e15f70a4595a1ae2c94fc5f1206c87db9359f
김민지 <twoji3019@naver.com>
2025-04-27 12:04:30 +0900

    입력값 확인

--- routes/auth.js
+++ routes/auth.js
@@ -8,8 +8,16 @@ const router = express.Router();
 
 // 로그인 라우터
 router.post('/login',(req,res)=>{
+
+  //입력값
+  const {text,password} = req.body
   
-  // 입력값체크
+  // 입력값체크 - 입력값이 있어야함
+  if(!text || !password){
+    return res.status(400).json({message:'텍스트와 비밀번호를 입력'})
+
+  }
+  
   // DB에서 사용자 찾기
   // 비밀번호 비교
   // 토큰 발급
