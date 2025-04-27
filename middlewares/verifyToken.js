aa0a6bb860a98cae50b6c6f9f4975dfd5647eb8e
김민지 <twoji3019@naver.com>
2025-04-27 13:12:44 +0900

    토큰 존재 확인

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -3,8 +3,14 @@ import jwt from 'jsonwebtoken'
 
 //함수 작성
 export function verifyToken(req,res,next){
+
+  //토큰
+  const authHeader = req.headers.authorization
   
   //토큰 존재 확인
+  if(!authHeader){
+    return res.status(401).json({message:'토큰 없음'})
+  }
   
   //토큰만 추출
   
