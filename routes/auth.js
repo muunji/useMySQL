c8b9bc477fa041240fd0d2627ac0cf0e7402b2d2
김민지 <twoji3019@naver.com>
2025-04-27 12:35:36 +0900

    사용자 찾기

--- routes/auth.js
+++ routes/auth.js
@@ -15,10 +15,18 @@ router.post('/login',(req,res)=>{
   // 입력값체크 - 입력값이 있어야함
   if(!text || !password){
     return res.status(400).json({message:'텍스트와 비밀번호를 입력'})
-
   }
   
-  // DB에서 사용자 찾기
+  // DB에서 사용자 찾기 - DB에서 조회
+  pool.execute('SELECT * FROM test WHERE text=?',[text],(err,results)=>{
+    //에러 발생시
+    if(err) return res.status(500).json({message:'DB 조회 실패',error:err})
+
+    //입력값이 0
+    if(results.length===0){
+      return res.status(404).json({message:'사용자 찾을 수 없음'})
+    }
+  })
   // 비밀번호 비교
   // 토큰 발급
 })
