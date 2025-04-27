5c9fd109032ef71dfd7e488b817439d15b53a2b9
김민지 <twoji3019@naver.com>
2025-04-27 12:42:19 +0900

    비밀번호 비교하기

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,10 @@ import express from 'express'
 import bycrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import pool from '../server/connect.js'
+import dotenv from 'dotenv'
 // 미들웨어
 const router = express.Router();
+dotenv.config()
 
 // 로그인 라우터
 router.post('/login',(req,res)=>{
@@ -26,9 +28,33 @@ router.post('/login',(req,res)=>{
     if(results.length===0){
       return res.status(404).json({message:'사용자 찾을 수 없음'})
     }
+
+    //유저 값 할당
+    const user = results[0]
   })
+
   // 비밀번호 비교
-  // 토큰 발급
+  bycrypt.compare(password, uesr.password,(err,isMatch)=>{
+    //에러
+    if(err) return res.status(500).json({message:'비밀번호 비교 실패',error:err})
+
+    //값 false
+    if(!isMatch){
+      return res.status(401).json({message:'비밀번호 불일치'})
+    }
+
+    // 토큰 발급
+    const token = jwt.sign(
+      {id:user.id, text:user.text}, //토큰에 담을 내용
+      process.env.JWT_SECRET, //.env 비밀키 사용
+      {expiresIn:'1h'} // 1시간 후 만료
+    )
+
+    res.json({message:'로그인 성공', token}) // 로그인 성공
+
+  })
+
+
 })
 
 
