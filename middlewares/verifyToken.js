65ee21755fc25bed1d936a84206fbb3c0d887e43
김민지 <twoji3019@naver.com>
2025-04-27 13:07:46 +0900

    뼈대 작성

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,9 +1,12 @@
 // 모듈 가져오기
+import jwt from 'jsonwebtoken'
 
 //함수 작성
-
-//토큰 존재 확인
-
-//토큰만 추출
-
-//토큰 검증
+export function verifyToken(req,res,next){
+  
+  //토큰 존재 확인
+  
+  //토큰만 추출
+  
+  //토큰 검증
+}
